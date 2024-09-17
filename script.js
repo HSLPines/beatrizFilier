af89d7de9019641e744429edec1d277d8150883b
Henrique Lecce <henrique.lecce.73@gmail.com>
2024-09-17 14:30:14 -0300

    DOCS

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if (top >= offset && top < offset + height) {
+        if (top >= offset && top < offset + height){
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
